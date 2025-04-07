42acb685e4b01b9f0e548ddc4ea3c66fc5f3a1b1
Wisut-K <52121452+Wisut-K@users.noreply.github.com>
2025-04-07 14:24:38 +0000

    Try_Pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,6 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import requests
-import psycopg2
 
-symbol = st.sidebar.text_input("Symbol", value="APPL", max_chars=5)
-option = st.sidebar.selectbox("Which dashboard?",("chart","Pattern","Wallstreetbets"))
-
-connection = psycopg2.connect(host=cocnfig.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB+PASS)
-
-st.header(option)
-
-
-if option == "Chart":
-    st.subheader("Chart Dashboard")
-
-if option == "Pattern":
-    st.subheader("Pattern Dashboard")
-
-
-if option == "Wallstreetbets":
-    st.subheader("Wallstreetbets Dashboard")
-
-    
+# Define the pages
+main_page = st.Page("main_page.py", title="Main Page", icon="🎈")
+page_2 = st.Page("page_2.py", title="Page 2", icon="❄️")
+page_3 = st.Page("page_3.py", title="Page 3", icon="🎉")
