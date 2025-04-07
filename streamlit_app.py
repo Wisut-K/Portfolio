95a84763aa52413acefaf2a5605a8742f39e0fab
Wisut-K <52121452+Wisut-K@users.noreply.github.com>
2025-04-07 13:48:30 +0000

    Test_Dashbaord_Code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,25 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import psycopg2
 
-st.write("Hello world")
+symbol = st.sidebar.text_input("Symbol", value="APPL", max_chars=5)
+option = st.sidebar.selectbox("Which dashboard?",("chart","Pattern","Wallstreetbets"))
+
+connection = psycopg2.connect(host=cocnfig.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB+PASS)
+
+st.header(option)
+
+
+if option == "Chart":
+    st.subheader("Chart Dashboard")
+
+if option == "Pattern":
+    st.subheader("Pattern Dashboard")
+
+
+if option == "Wallstreetbets":
+    st.subheader("Wallstreetbets Dashboard")
+
+    
